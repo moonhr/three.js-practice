9867d49df29698a162f6af4047001ef1eb8ab1e2
moonhr <moonhr153@gmail.com>
2024-06-16 21:58:34 +0900

    장면추가

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({
+  canvas : document.querySelector('#canvas')
+})
