edff277955012ba166258fcf61066064f2b0258d
moonhr <moonhr153@gmail.com>
2024-06-16 22:27:26 +0900

    모델링 불러오기 성공

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
-import { send } from 'vite';
+// import { GLTFLoader } from 'GLTFLoader';
 
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({
-  canvas : document.querySelector('#canvas')
-})
+let scene = new THREE.Scene();
+let renderer = new THREE.WebGLRenderer({
+  canvas : document.getElementById('canvas'),
+  antialias : true
+});
+renderer.outputEncoding = THREE.sRGBEncoding;
 
-const camera = new THREE.PerspectiveCamera(30, 1);
-camera.position.set(0,0,5);
+let camera = new THREE.PerspectiveCamera(30, 1);
+camera.position.set(0,0,10);
 
 //* 배경&조명
 scene.background = new THREE.Color('white');
-const light = new THREE.DirectionalLight(0xffff00, 10);
+let light = new THREE.DirectionalLight(0xffff00, 10);
 scene.add(light);
 
-const loader = new GLTFLoader();
-loader.load('./깜박이.glb', function(glb){
-  scene.add(glb.scene);
-  renderer.render(scene);
+let loader = new GLTFLoader();
+loader.load('./ccamback.glb', function(gltf){
+  scene.add(gltf.scene);
+  
+  function animate(){
+    requestAnimationFrame(animate)
+    gltf.scene.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
+  animate()
 });
+//마우스 컨트롤은 three.js OrbitControl
 
-renderer.render(scene);
+// renderer.render(scene);
 
