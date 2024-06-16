f89f188109aaa1844d7430298bc3fc959e89dcd3
moonhr <moonhr153@gmail.com>
2024-06-16 22:00:53 +0900

    3d 모델링 연결

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({
   canvas : document.querySelector('#canvas')
 })
+const loader = new GLTFLoader();
+loader.load('./깜박이.glb');
+
+renderer.render(scene);
+
