3d63e31125351077f089c6ba52302e4e6a37c400
moonhr <moonhr153@gmail.com>
2024-06-16 22:02:46 +0900

    카메라 세팅

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import { send } from 'vite';
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({
   canvas : document.querySelector('#canvas')
 })
+
+const camera = new THREE.PerspectiveCamera(30, 1);
+camera.position.set(0,0,5);
+
 const loader = new GLTFLoader();
-loader.load('./깜박이.glb');
+loader.load('./깜박이.glb', function(glb){
+  scene.add(glb.scene);
+  renderer.render(scene);
+});
 
 renderer.render(scene);
 
